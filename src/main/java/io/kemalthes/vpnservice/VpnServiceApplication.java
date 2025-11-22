3cc6454b65e8005b2246e18992c35d8002934886
kemalthes <stop1qq@gmail.com>
2025-11-22 19:46:09 +0300

    init commit

---
+++ src/main/java/io/kemalthes/vpnservice/VpnServiceApplication.java
@@ -0,0 +1,13 @@
+package io.kemalthes.vpnservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VpnServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VpnServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/kemalthes/vpnservice/VpnServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.kemalthes.vpnservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VpnServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
