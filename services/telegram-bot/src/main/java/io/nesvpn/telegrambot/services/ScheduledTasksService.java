db4ba4db05c6c747ef6d455f4b1c641aeaf8db4c
Latnove <r1ghtnowcp2@gmail.com>
2026-03-08 01:26:08 +0300

    add cron in 12:00 every day for notify message when user subscriptions getting smaller 2 days

--- services/telegram-bot/src/main/java/io/nesvpn/telegrambot/services/ScheduledTasksService.java
+++ services/telegram-bot/src/main/java/io/nesvpn/telegrambot/services/ScheduledTasksService.java
@@ -2,12 +2,15 @@ package io.nesvpn.telegrambot.services;
 
 import io.nesvpn.telegrambot.handler.MessageHandler;
 import io.nesvpn.telegrambot.model.Payment;
+import io.nesvpn.telegrambot.model.Token;
 import io.nesvpn.telegrambot.model.User;
+import io.nesvpn.telegrambot.util.Formatter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -20,9 +23,9 @@ import java.util.stream.Collectors;
 public class ScheduledTasksService {
 
     private final PaymentService paymentService;
-    private final TonPaymentService tonPaymentService;
     private final MessageHandler messageHandler;
     private final UserService userService;
+    private final TokenService tokenService;
 
     @Scheduled(fixedRate = 300000)
     public void checkExpiredPayments() {
@@ -65,6 +68,24 @@ public class ScheduledTasksService {
             if (user != null) {
                 messageHandler.showSuccessPayment(user.getTgId(), payment, user);
             }
+        }
+    }
+
+    @Scheduled(cron = "0 0 12 * * *")
+    public void notifyExpiringSubscriptions() {
+        LocalDateTime now = LocalDateTime.now();
+        LocalDateTime twoDaysLater = now.plusDays(2);
+
+        List<Token> tokens = tokenService.getExpiringTokens(now, twoDaysLater);
+
+        for (Token token : tokens) {
+
+            User user = userService.getUserById(token.getUserId());
+
+            messageHandler.showSubscriptionExpiring(
+                    user.getTgId(),
+                    Formatter.formatMoscow(token.getValidTo())
+            );
         }
     }
 }
